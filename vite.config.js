3906c43771429b14b1b03728d03c25bef266e1c0
Wizard-Mayank <30517csaiml@gmail.com>
2025-06-20 21:22:23 +0530

    fix: Vercel path base corrected

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// Correct GitHub repo name
-const repoName = 'My-Portfolio'; // ✅ THIS is your actual repo name
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: `/${repoName}/`, // ✅ Always serve from this path on GitHub Pages
   plugins: [react()],
-})
+  base: '/', // ✅ Correct for Vercel deployment
+});
